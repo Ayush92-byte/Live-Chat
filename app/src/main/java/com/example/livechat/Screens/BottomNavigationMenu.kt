6020424070ac148099ca67a877fdeb5f01092ebc
Ayush <ayushkushwaha2212@gamil.com>
2024-01-27 15:51:48 +0530

    first commit

---
+++ app/src/main/java/com/example/livechat/Screens/BottomNavigationMenu.kt
@@ -0,0 +1,90 @@
+package com.example.livechat.Screens
+
+import android.annotation.SuppressLint
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.example.livechat.DestinationScreen
+import com.example.livechat.R
+import com.example.livechat.navigateTo
+import com.exyte.animatednavbar.AnimatedNavigationBar
+import com.exyte.animatednavbar.animation.balltrajectory.Parabolic
+import com.exyte.animatednavbar.animation.indendshape.Height
+import com.exyte.animatednavbar.animation.indendshape.shapeCornerRadius
+
+enum class BottomNavigationItem(val icon: Int, val navDestination: DestinationScreen) {
+
+    CHATLIST(R.drawable.chaticon, DestinationScreen.ChatList),
+    STATUSLIST(R.drawable.status, DestinationScreen.StatusList),
+    PROFILE(R.drawable.user, DestinationScreen.Profile)
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun BottomNavigationMenu(
+
+    selectedItem: BottomNavigationItem,
+    navController: NavController
+) {
+
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+            .padding(top = 4.dp)
+            .background(Color.White)
+    ) {
+
+        for (item in BottomNavigationItem.values()) {
+            Image(
+                painter = painterResource(id = item.icon),
+                contentDescription = null,
+                modifier = Modifier
+                    .size(40.dp)
+                    .padding(4.dp)
+                    .weight(1f)
+                    .clickable {
+                        navigateTo(navController, item.navDestination.route)
+                    },
+                colorFilter = if (item == selectedItem)
+                ColorFilter.tint(color = Color.Black)
+                else
+                ColorFilter.tint(Color.Gray)
+                )
+        }
+    }
+
+
+
+    }
+
+
